1c6f19d4c03c4876e13c725b9fe6ffaafe9ac5ae
massarrahelenna <helennamassarrapaes@.com>
2026-06-20 18:14:06 -0300

    feat: adiciona modelo de dados Contato

--- src/models.py
+++ src/models.py
@@ -2,3 +2,15 @@
 modelo de dados pra contato
 """
 from dataclasses import dataclass
+
+@dataclass (frozen=True)
+class Contato:
+    nome:str
+    telefone:str
+    
+    @staticmethod
+    def from_dict(data: dict) -> 'Contato':
+        return Contato(
+            nome=str(data['nome']).strip(),
+            telefone=str(data['telefone']).strip()
+        )
